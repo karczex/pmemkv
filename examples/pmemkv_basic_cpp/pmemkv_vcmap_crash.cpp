ccd6707e5291a1d0186c8e8ba359acbfee77a464
Pawel Karczewski <pawel.karczewski@intel.com>
2020-10-05 14:48:02 +0200

    tweaks in bug reproducer

--- examples/pmemkv_basic_cpp/pmemkv_vcmap_crash.cpp
+++ examples/pmemkv_basic_cpp/pmemkv_vcmap_crash.cpp
@@ -12,13 +12,12 @@
 #include <sstream>
 
 #include <numeric>
-#include <vector>
 #include <thread>
+#include <vector>
 
 #define LOG(msg) std::cout << msg << std::endl
 
 using namespace pmem::kv;
-
 
 static inline pmem::kv::string_view uint64_to_strv(uint64_t &key)
 {
@@ -40,28 +39,27 @@ void parallel_exec(size_t threads_number, Function f)
 	}
 }
 
-
 const uint64_t SIZE_16M = 16UL * 1024UL * 1024UL;
 /*
- * /opt/workspace/pmemkv_upstream/build/tests/concurrent_put_get_remove_single_op_params vcmap;{"path":"/dev/shm/vcmap__concurrent_put_get_remove_single_op_params__default_1000_0_none","size":104857600};1000
+ * /opt/workspace/pmemkv_upstream/build/tests/concurrent_put_get_remove_single_op_params
+ * vcmap;{"path":"/dev/shm/vcmap__concurrent_put_get_remove_single_op_params__default_1000_0_none","size":104857600};1000
  */
 int main(int argc, char *argv[])
 {
 
-	LOG("Creating config");
+//	LOG("Creating config");
 	config cfg;
 
-	status s = cfg.put_string("path" , "/dev/shm");
+	status s = cfg.put_string("path", "/dev/shm");
 	assert(s == status::OK);
 	s = cfg.put_uint64("size", SIZE_16M);
 	assert(s == status::OK);
 
 	const int threads_number = 16;
 
-	LOG("test with " << threads_number << " threads");
+//	LOG("test with " << threads_number << " threads");
 
-
-	LOG("Opening pmemkv database with 'vcmap' engine");
+//	LOG("Opening pmemkv database with 'vcmap' engine");
 	db *kv = new db();
 	assert(kv != nullptr);
 	s = kv->open("vcmap", std::move(cfg));
@@ -71,16 +69,15 @@ int main(int argc, char *argv[])
 	std::iota(keys.begin(), keys.end(), 0);
 
 	parallel_exec(threads_number, [&](size_t thread_id) {
-			LOG("thread: " << thread_id);
-			auto s = kv->put(uint64_to_strv(keys[thread_id]),
-					   uint64_to_strv(keys[thread_id]));
-			if (static_cast<int>(s) != 0) 
-			{
-				LOG("status: " << static_cast<int>(s) << " ERROR occured");
-			}
+		auto s = kv->put(uint64_to_strv(keys[thread_id]),
+				 uint64_to_strv(keys[thread_id]));
+		if (static_cast<int>(s) != 0) {
+			LOG("status: " << static_cast<int>(s) << " ERROR occured");
+			exit(1);
+		}
 	});
 
-	LOG("Closing database");
+//	LOG("Closing database");
 	delete kv;
 
 	return 0;
